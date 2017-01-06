cb4c239cda588c436994880461a7171e1077f8c6
Erik Uecke <erik.uecke@digitalfilmacademy.edu>
2017-01-06 13:59:22 -0500

    Set Up Constants and CurreWeather vars

---
+++ RainyShinyCloudy/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  RainyShinyCloudy
+//
+//  Created by Erik Uecke on 1/6/17.
+//  Copyright © 2017 Erik Uecke. All rights reserved.
+//
+
+import Foundation
+
+let BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
+
+let LATITUDE = "lat="
+let LONGITUDE = "&lon="
+let APP_ID = "&appid="
+let API_KEY = "f03f7ccd80405068eeb6343b94d744d4"
+
+let CURRENT_WEATHER_URL = "\(BASE_URL)\(LATITUDE)-36\(LONGITUDE)123\(APP_ID)\(API_KEY)"
+
